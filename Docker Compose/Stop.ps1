d52ae69f0d10b47d282becddb28b24a24d3f6d0b
Martin Freiwald <martin@freedom-manufaktur.com>
2025-11-18 11:13:48 +0100

    update docker compose docu

---
+++ Docker Compose/Stop.ps1
@@ -0,0 +1,9 @@
+﻿# Stop
+if ((Test-Path -Path "compose.env.user"))
+{
+  docker compose --env-file ./compose.env --env-file ./compose.env.user down
+}
+else
+{
+  docker compose --env-file ./compose.env down
+}
