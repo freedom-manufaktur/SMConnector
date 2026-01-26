b518873efe678a31622a3073c6b446b4ffab3792
Mertsch <Mertsch@users.noreply.github.com>
2026-01-26 23:20:05 +0100

    Update HELM chart (#18)

--- HELM Chart/Connect.ps1
+++ HELM Chart/Connect.ps1
@@ -1,6 +1,5 @@
-﻿#                                                                           ReleaseName                             ReleaseName
-$POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=sm-connector,app.kubernetes.io/instance=sm-connector" -o jsonpath="{.items[0].metadata.name}")
-$CONTAINER_PORT=$(kubectl get pod --namespace default $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}")
-Write-Output "Application:  http://localhost:8010"
-Write-Output "Health check: http://localhost:8010/healthcheck"
-kubectl --namespace default port-forward $POD_NAME 8010:$CONTAINER_PORT
+﻿$PublicPort = 8000
+Write-Output "Application:  http://localhost:$PublicPort/"
+Write-Output "Debug:        http://localhost:$PublicPort/Debug"
+Write-Output "Health check: http://localhost:$PublicPort/healthcheck"
+kubectl --namespace default port-forward svc/sm-connector ${PublicPort}:8080
